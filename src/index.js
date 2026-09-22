fc12f921aa135075ae61fbc352b6277cdce68271
ApplePair111 <applepair111@applepair111.xyz>
2026-09-22 19:21:20 +0300

    initial commit cuz im a sucker

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// modules
+
+const express = require("express");
+const https = require("https");
+const axios = require("axios");
+
+// setup Express
+
+const app = express();
+
+app.use(express.json);
+
+app.post("/login", async (req, res) => {
+    
+});
