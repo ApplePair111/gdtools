8e12751643a6916ac608a0d9cd66451b59d40035
ApplePair111 <applepair111@applepair111.xyz>
2026-09-23 18:48:16 +0300

    ESMIFICATION MORE

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.post("/api/login", async (req, res) => {
 
     console.log("Recieved request for login!")
 
-    const { data, status } = await axios.post("https://boomlings.com/database/accounts/loginGJAccount.php", new URLSearchParams({ udid: "58121f94-8233-4e50-9095-052a7241b774", userName: req.body.username, gjp2: require('crypto').createHash('sha1').update(req.body.password + 'mI29fmAnxgTs').digest('hex'), secret: "Wmfv3899gc9" }), { headers: { 'User-Agent': false } }) // HUGE ONELINER!
+    const { data, status } = await axios.post("https://boomlings.com/database/accounts/loginGJAccount.php", new URLSearchParams({ udid: "58121f94-8233-4e50-9095-052a7241b774", userName: req.body.username, gjp2: process.getBuiltinModule('crypto').createHash('sha1').update(req.body.password + 'mI29fmAnxgTs').digest('hex'), secret: "Wmfv3899gc9" }), { headers: { 'User-Agent': false } }) // HUGE ONELINER!
 
     try {data = data.split(",")} catch {res.status(500).send("GD servers are bad")}
 
