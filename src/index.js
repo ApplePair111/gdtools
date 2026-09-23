f02971967991e8d9637000c58dc2bd892a2a8f58
ApplePair111 <applepair111@applepair111.xyz>
2026-09-23 17:34:35 +0300

    test1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // modules
 
 const express = require("express");
-const https = require("https");
 const axios = require("axios");
 
 // setup Express
@@ -10,6 +9,15 @@ const app = express();
 
 app.use(express.json);
 
-app.post("/login", async (req, res) => {
-    
+app.post("/api/login", async (req, res) => {
+    const { data, status } = await axios.post("https://boomlings.com/database/accounts/loginGJAccount.php", new URLSearchParams({ udid: "58121f94-8233-4e50-9095-052a7241b774", userName: req.body.username, gjp2: require('crypto').createHash('sha1').update(req.body.password + 'mI29fmAnxgTs').digest('hex'), secret: "Wmfv3899gc9" })) // HUGE ONELINER!
+
+    try {data = data.split(",")} catch {res.status(500).send("GD servers are bad")}
+
+    if (!status == 200) {res.status(500).send("GD servers are bad")}
+
+    res.json({ ok: true, accountID: data[0], playerID: data:[1]})
+
 });
+
+app.listen(3000)
