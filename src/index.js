e04d117e3f829a7a6a70ddc3eef9c101fc3f4edf
ApplePair111 <applepair111@applepair111.xyz>
2026-09-23 18:18:50 +0300

    fiX

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(express.json);
 
 app.post("/api/login", async (req, res) => {
+
+    console.log("Recieved request for login!")
+    
     const { data, status } = await axios.post("https://boomlings.com/database/accounts/loginGJAccount.php", new URLSearchParams({ udid: "58121f94-8233-4e50-9095-052a7241b774", userName: req.body.username, gjp2: require('crypto').createHash('sha1').update(req.body.password + 'mI29fmAnxgTs').digest('hex'), secret: "Wmfv3899gc9" }), { headers: { 'User-Agent': false } }) // HUGE ONELINER!
 
     try {data = data.split(",")} catch {res.status(500).send("GD servers are bad")}
