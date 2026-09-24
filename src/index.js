6abfe5bbedaeac12d20f6e6b9ef3e257f7acbd11
ApplePair111 <applepair111@applepair111.xyz>
2026-09-24 06:21:49 +0300

    debugging

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.post("/api/login", async (req, res) => {
 
     //const { data, status } = await axios.post("http://localhost:3001", new URLSearchParams({ udid: "58121f94-8233-4e50-9095-052a7241b774", userName: req.body.username, gjp2: process.getBuiltinModule('crypto').createHash('sha1').update(req.body.password + 'mI29fmAnxgTs').digest('hex'), secret: "Wmfv3899gc9" }), { headers: { 'User-Agent': false } }) // HUGE ONELINER!
 
-    
+    console.log(`GD request recieved with status ${status} and data ${data}`)
 
     try {data = data.split(",")} catch {console.error("bad data split"); return res.status(500).send("GD servers are bad")}
 
